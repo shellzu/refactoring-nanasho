9e16da28ff72ce1fe70e06ed39b3625526c0c917
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-10-10 11:49:20 +0900

    比較するロジックを追加

--- replace-primitive-with-object/main.js
+++ replace-primitive-with-object/main.js
@@ -10,8 +10,19 @@ class Order {
 class Priority {
     constructor(value) {
         if (value instanceof Priority) return value;
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}> is invalid for Priority`);
         this._value = value;
     }
+    toString() {return this._value;}
+    get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+    static legalValues() {returnb['low', 'normal', 'high', 'rush'];}
+
+    equals(other) {return this._index === other._index;}
+    higherThan(other) {return this._index > other._index;}
+    lowerThan(other) {return this._index < other._index;}
 }
 
 function getHighPriorityCount(orders) {
