c2bc0f7b184143d8f893441e0f726b75188c85d4
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-10-10 11:18:16 +0900

    変数のカプセル化

--- replace-primitive-with-object/main.js
+++ replace-primitive-with-object/main.js
@@ -3,6 +3,8 @@ class Order {
         this.priority = data.priority;
         // 初期化が続く
     }
+    get priority() { return this._priority; }
+    set priority(aString) { this._priority = aString; }
 }
 
 function getHighPriorityCount(orders) {
